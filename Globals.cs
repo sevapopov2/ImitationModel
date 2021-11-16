fec471a4ecb01e03dc61699c4f404d7de70ef2af
Vsevolod Popov <sevapopov13@gmail.com>
2021-11-16 11:18:27 +0300

    Fixed static variables and functions in globals class.

--- Globals.cs
+++ Globals.cs
@@ -2,28 +2,28 @@ namespace Model
 {
     public static class Globals
     {
-        private int SecInHour;
-        private int clientPerHour;
-        private double _clientFlowRate;
-        public double clientFlowRate
+        private static int SecInHour;
+        private static int clientPerHour;
+        private static double _clientFlowRate;
+        public static double clientFlowRate
         {
             get { return _clientFlowRate; }
             set { _clientFlowRate = value / SecInHour; }
         }
-        private float _serveFlowRate;
-        public float serveFlowRate {
+        private static float _serveFlowRate;
+        public static float serveFlowRate {
             get { return _serveFlowRate; }
-            set { _serveFlowRate = value }
+            set { _serveFlowRate = value; }
         }
-        private int _dT;
-        public int dT {
+        private static int _dT;
+        public static int dT {
             get { return _dT; }
             set { _dT = value; }
         }
-        private int _ratio;
-        public int ratio {
+        private static int _ratio;
+        public static int ratio {
             get { return _ratio; }
-            set { _ratio = dT / SecInHour }
+            set { _ratio = dT / SecInHour; }
         }
     }
 }
