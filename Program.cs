643256f56291cf1e452c887676d3038c285e7942
Vsevolod Popov <sevapopov13@gmail.com>
2021-11-28 18:41:15 +0300

    Added an attempt to quit and add channel by keypress.

--- Program.cs
+++ Program.cs
@@ -11,6 +11,16 @@ namespace Model
             Globals.clientFlowRate = 5/3600;
             Globals.serveFlowRate = 1;
             Globals.ratio = currentDateTime / Globals.dT;
+            int channelsCount = 4;
+             var symbol = Console.ReadKey();
+            if (symbol == "q") {
+                Environment.Exit(0);
+            }
+            if (symbol == ConsoleKeyInfo()) {
+                channelsCount = channelsCount +1;
+                Console.WriteLine("The channels count is set to", channelsCount);
+            }
         }
+
     }
 }
