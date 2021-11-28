ef8dd7b6002f39aa2698f67e3cfab4889233e343
Vsevolod Popov <sevapopov13@gmail.com>
2021-11-28 18:07:49 +0300

    An attempt to write a main method.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,11 @@ namespace Model
     {
         static void Main(string[] args)
         {
-            
+            DateTime currentDateTime = DateTime.Now;
+            Globals.dT = 5;
+            Globals.clientFlowRate = 5/3600;
+            Globals.serveFlowRate = 1;
+            Globals.ratio = currentDateTime / Globals.dT;
         }
     }
 }
