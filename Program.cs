98cec539fb6a140f79a5df88b7847ada98fdebae
Vsevolod Popov <sevapopov13@gmail.com>
2021-12-11 18:08:32 +0300

    Optimized class code. Changed ratio variable to work as required.

--- Program.cs
+++ Program.cs
@@ -6,20 +6,12 @@ namespace Model
     {
         static void Main(string[] args)
         {
-            DateTime currentDateTime = DateTime.Now;
+string command = "";
             Globals.dT = 5;
-            Globals.clientFlowRate = 5/3600;
+            Globals.clientFlowRate = 5/Globals.SecInHour;
             Globals.serveFlowRate = 1;
-            Globals.ratio = currentDateTime / Globals.dT;
-            int channelsCount = 4;
-             var symbol = Console.ReadKey();
-            if (symbol == "q") {
-                Environment.Exit(0);
-            }
-            if (symbol == ConsoleKeyInfo()) {
-                channelsCount = channelsCount +1;
-                Console.WriteLine("The channels count is set to", channelsCount);
-            }
+            Globals.ratio = Globals.dT / Globals.SecInHour;
+            public int channelsCount = 0;
         }
 
     }
